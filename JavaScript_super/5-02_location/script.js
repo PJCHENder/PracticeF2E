8119de05e76a8dac5be8b875151a8369f4b2ae80
PJCHENder <pjchender@gmail.com>
2016-09-09 14:55:07 +0800

    5-02_location 完成

--- JavaScript_super/5-02_location/script.js
+++ JavaScript_super/5-02_location/script.js
@@ -1,3 +1,15 @@
+var lang = document.querySelector('html').lang;
+
+var opt;
+if(lang == 'ja'){
+    opt = document.querySelector('option[value="index_ja.html"]');
+}else if(lang == 'en'){
+    opt = document.querySelector('option[value="index_en.html"]');
+}else if(lang == 'zh'){
+    opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
 document.getElementById('form').select.onchange = function(){
     location.href = document.getElementById('form').select.value;
 }
