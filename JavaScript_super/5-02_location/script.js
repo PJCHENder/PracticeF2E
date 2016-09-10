6e3a4dd49928848157a6320280cf9e5985ef4183
PJCHENder <pjchender@gmail.com>
2016-09-10 17:26:46 +0800

    5-02_location 將 if else 改成用 switch 表示

--- JavaScript_super/5-02_location/script.js
+++ JavaScript_super/5-02_location/script.js
@@ -1,12 +1,27 @@
 var lang = document.querySelector('html').lang;
 
 var opt;
+
+/*
 if(lang == 'ja'){
     opt = document.querySelector('option[value="index_ja.html"]');
 }else if(lang == 'en'){
     opt = document.querySelector('option[value="index_en.html"]');
 }else if(lang == 'zh'){
     opt = document.querySelector('option[value="index.html"]');
+}
+*/
+
+switch(lang){
+    case 'ja':
+        opt = document.querySelector('option[value="index_ja.html"]');
+        break;
+    case 'en':
+        opt = document.querySelector('option[value="index_en.html"]');
+        break;
+    case 'zh':
+        opt = document.querySelector('option[value="index.html"]');
+        break;
 }
 opt.selected = true;
 
