2ba402c93aec909931bf09d4e262c214b9214aa1
PJCHENder <pjchender@gmail.com>
2016-09-11 22:12:35 +0800

    7-02_photo 將程式顯示於網頁上

--- JavaScript_super/7-02_photo/app.js
+++ JavaScript_super/7-02_photo/app.js
@@ -14,6 +14,22 @@ $(document).ready(function(){
         .done(function(data){
             photoData = data;
             console.log(photoData);
+            
+            $(photoData.data).each(function(){
+                var caption = '';
+                if(this.caption){
+                    caption = this.caption.text;
+                }
+                
+                $('#gallery').append(
+                    $('<div class="img_block"></div>').append(
+                        $('<a></a>').attr('href',this.link).attr('target','_blank').append(
+                            $('<img>').attr('src',this.images.low_resolution.url)
+                        )
+                    ).append($('<p class="caption"></p>').text(caption + '讚' + this.likes.count))
+                )
+            })
+            
         })
         .fail(function(){
             $('#gallery').text(textStatus);
